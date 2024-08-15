deca6d060890a929f7a14db769ba8ab0886dbdd5
Alvin <alvinngg9@gmail.com>
2024-08-14 17:25:19 -0700

    Update Solution.java

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -4,19 +4,29 @@ import java.util.HashMap;
 
 public class Solution {
     public int findMaxLength(int[] nums) {
-        if (nums.length == 0) return 0;
+        //initialize hashmap of key integer and value integer
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) nums[i] = -1;
-        }
-        int sum = 0;
         int result = 0;
-        map.put(0, -1);
+        //convert all values of 0 to -1
         for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                nums[i] = -1;
+            }
+        }
+        //add the base case
+        map.put(0, -1);
+        int sum = 0;
+        //iterate through the array
+        for (int i = 0; i < nums.length; i++) {
+            //find the sum up to that index
             sum += nums[i];
+            //check if the map already has the current sum
             if (map.containsKey(sum)) {
+                //if the map has seen that sum, find the longest length of the subarray with
+                //0 and 1
                 result = Math.max(result, i - map.get(sum));
             } else {
+                //add the sum and the current index to the map
                 map.put(sum, i);
             }
         }
